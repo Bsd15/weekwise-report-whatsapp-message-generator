be1468772941ca493d3fa5a332589d857f59fe5f
bsd <datta.bathina15@gmail.com>
2020-06-27 22:39:34 +0530

    Removed lorem ipsum

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat quae,
-      nostrum animi dignissimos repudiandae cupiditate corrupti? Aliquid sunt
-      perferendis hic provident ipsa, corrupti nihil recusandae accusamus
-      necessitatibus officia fugit omnis.
+      React with typescript!!!
     </div>
   );
 }
