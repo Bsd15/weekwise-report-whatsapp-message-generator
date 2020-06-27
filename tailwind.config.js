6976b3aa7b967563a6f35be3ea4cded36e3c6764
bsd <datta.bathina15@gmail.com>
2020-06-27 22:54:32 +0530

    Added container settings. Made container center and have padding by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: [],
+	theme: {
+		container: {
+			center: true,
+			padding: {
+				default: "1rem",
+				sm: "2rem",
+				lg: "4rem",
+				xl: "5rem",
+			},
+		},
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
