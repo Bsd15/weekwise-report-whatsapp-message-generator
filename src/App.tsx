6829ec911a575a9d3370a8090584a1e9acefb1c3
bsd <datta.bathina15@gmail.com>
2020-06-28 15:04:30 +0530

    Added routing for ManageStudents

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+import ManageStudents from "./components/ManageStudents/ManageStudents";
 import "./App.css";
 
 function App() {
-	return <div className="App">React with typescript!!!</div>;
+	return (
+		<div className="container">
+			<NavLink to="/manageStudents">Manage Students</NavLink>
+			<Switch>
+				<Route path="/manageStudents">
+					<ManageStudents />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
