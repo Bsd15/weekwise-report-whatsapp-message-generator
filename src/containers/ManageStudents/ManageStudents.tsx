c914e53ea0ecc0360c22fe5ad52d9a1d288dffaa
bsd <datta.bathina15@gmail.com>
2020-06-28 20:31:34 +0530

    Added Input of type select for classes

--- src/containers/ManageStudents/ManageStudents.tsx
+++ src/containers/ManageStudents/ManageStudents.tsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from "react";
 import firebase from "../../firebase";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import Input from "../../components/UI/Input/Input";
+
 const ManageStudents = () => {
-	const [classes, setClasses] = useState();
+	const [classes, setClasses] = useState<Array<object>|undefined>();
 	useEffect(() => {
 		const itemRef = firebase.database().ref("classes");
 		itemRef.on("value", (snapshot) => setClasses(snapshot.val()));
 	}, []);
+
+	const selectOptions: Option[] = [];
+	if (classes) {
+		Object.entries(classes).forEach(([key, value])=>{
+			selectOptions.push({
+				displayName: key,
+				value: value
+			})
+		})
+	}
 
 	return (
 		<div>
 			{classes ? (
 				<div>
 					Manage Students
-					<p>{classes}</p>
+					<Input type="select" name="class" label="Class" options={selectOptions} />
 				</div>
 			) : (
 				<Spinner />
