1fe14c40ecca5ca2927f70737ec6887cb738b17a
bsd <datta.bathina15@gmail.com>
2020-06-28 22:24:49 +0530

    Formatted code

--- src/containers/ManageStudents/ManageStudents.tsx
+++ src/containers/ManageStudents/ManageStudents.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import firebase from "../../firebase";
-import Spinner from "../../components/UI/Spinner/Spinner";
-import Input from "../../components/UI/Input/Input";
+import React, { useEffect, useState } from 'react';
+import firebase from '../../firebase';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import Input from '../../components/UI/Input/Input';
 
 const ManageStudents = () => {
 	const [classes, setClasses] = useState<Array<object> | undefined>();
 	useEffect(() => {
-		const itemRef = firebase.database().ref("classes");
-		itemRef.on("value", (snapshot) => setClasses(snapshot.val()));
+		const itemRef = firebase.database().ref('classes');
+		itemRef.on('value', (snapshot) => setClasses(snapshot.val()));
 	}, []);
 
 	const selectOptions: Option[] = [];
@@ -20,16 +20,26 @@ const ManageStudents = () => {
 		});
 	}
 
+	const [selectedClass, setSelectedClass] = useState(
+		classes ? classes[0]['3'] : ''
+	);
+
+	const onChangeHandler = (value) => {
+		setSelectedClass(value);
+	};
+
 	return (
 		<div>
 			{classes ? (
 				<div>
-					<h1>Manage classes for {}</h1>
+					<h1>Manage classes for {selectedClass}</h1>
 					<Input
 						type="select"
 						name="class"
 						label="Class"
 						options={selectOptions}
+						value={selectedClass}
+						onChangeHandler={onChangeHandler}
 					/>
 				</div>
 			) : (
