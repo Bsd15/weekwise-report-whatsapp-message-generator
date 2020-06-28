251d7b312cd89f8ed05c0af1aaebcadee1ce19c6
bsd <datta.bathina15@gmail.com>
2020-06-28 17:27:19 +0530

    Changed text to css based spinner

--- src/components/UI/Spinner/Spinner.tsx
+++ src/components/UI/Spinner/Spinner.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import classes from "./Spinner.module.css";
 const Spinner = () => {
-	return <div>Loading....</div>;
+	return (
+		<div className={classes.skChase}>
+			<div className={classes.skChaseDot}></div>
+			<div className={classes.skChaseDot}></div>
+			<div className={classes.skChaseDot}></div>
+			<div className={classes.skChaseDot}></div>
+			<div className={classes.skChaseDot}></div>
+			<div className={classes.skChaseDot}></div>
+		</div>
+	);
 };
 
 export default Spinner;
