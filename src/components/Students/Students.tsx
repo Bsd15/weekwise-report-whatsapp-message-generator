7b86db27e86b88c79f1de3901fd29efac18c949b
bsd <datta.bathina15@gmail.com>
2020-07-02 15:20:54 +0530

    Added prop error

--- src/components/Students/Students.tsx
+++ src/components/Students/Students.tsx
@@ -4,12 +4,9 @@ import Spinner from '../UI/Spinner/Spinner';
 const Students = (props: {
 	students: Array<object> | string;
 	areLoading: boolean;
+	error: string | undefined;
 }) => {
-	return (
-		<div>
-			{!props.areLoading ? JSON.stringify(props.students) : <Spinner />}
-		</div>
-	);
+	return <div>{props.areLoading && <Spinner />}</div>;
 };
 
 export default Students;
