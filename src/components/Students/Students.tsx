f6eddd4434c6055b3bbdc8047c97966ec549b522
bsd <datta.bathina15@gmail.com>
2020-07-02 15:44:24 +0530

    Added conditional rendering for spinner, error and students

--- src/components/Students/Students.tsx
+++ src/components/Students/Students.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Spinner from '../UI/Spinner/Spinner';
+import Student from './Student/Student';
 
 const Students = (props: {
 	students: Array<object> | string;
 	areLoading: boolean;
 	error: string | undefined;
 }) => {
-	return <div>{props.areLoading && <Spinner />}</div>;
+	let students: any;
+	if (props.error) {
+		students = <p>{props.error}</p>;
+	} else {
+		students = Object.values(props.students).map((studentName, i) => <Student key={i} name={studentName} />)
+	}
+	return <section>{props.areLoading ? <Spinner /> : students}</section>;
 };
 
 export default Students;
