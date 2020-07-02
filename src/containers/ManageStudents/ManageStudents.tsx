2df3c7e3c40c2abedcaa56d84ab5d5f56bc567f1
bsd <datta.bathina15@gmail.com>
2020-07-02 14:51:16 +0530

    Optimized useEffect to fetch and set classes. Created memoized function to fetch students based on the selected class. Moved the If condition that sets the fetched classes as selectedOption to useEffect.

--- src/containers/ManageStudents/ManageStudents.tsx
+++ src/containers/ManageStudents/ManageStudents.tsx
@@ -1,34 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import firebase from '../../firebase';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import Input from '../../components/UI/Input/Input';
+import Students from '../../components/Students/Students';
 
 const ManageStudents = () => {
 	const [classes, setClasses] = useState<Array<object> | undefined>();
 	const [selectedClass, setSelectedClass] = useState<string>('');
+	const [classStudents, setClassStudents] = useState<
+		Array<object> | undefined
+	>();
+	const [selectOptions, setSelectOptions] = useState<Option[]>([]);
+	const fetchClassStudents = useCallback((classNumber) => {
+		const classStudentsRef = firebase.database().ref(classNumber);
+		classStudentsRef.once('value', (snapshot) =>
+			setClassStudents(snapshot.val())
+		);
+	}, []);
+	
 	useEffect(() => {
 		const classesRef = firebase.database().ref('classes');
 		classesRef.on('value', (snapshot) => setClasses(snapshot.val()));
+		return () => {
+			classesRef.off('value');
+		};
 	}, []);
-
+	
 	useEffect(() => {
 		if (classes) {
-			setSelectedClass(Object.keys(classes)[0]);
-		}
-	}, [classes]);
-
-	const selectOptions: Option[] = [];
-	if (classes) {
-		Object.entries(classes).forEach(([key, value]) => {
-			selectOptions.push({
-				displayName: String(value),
-				value: key,
+			const newSelectOptions: Option[] = [];
+			Object.entries(classes).forEach(([key, value]) => {
+				newSelectOptions.push({
+					displayName: String(value),
+					value: key,
+				});
 			});
-		});
-	}
+			setSelectOptions(newSelectOptions);
+			const defaultClass = Object.keys(classes)[0];
+			setSelectedClass(defaultClass);
+			fetchClassStudents(defaultClass);
+		}
+	}, [classes, fetchClassStudents]);
 
 	const onChangeHandler = (value: string) => {
 		setSelectedClass(value);
+		fetchClassStudents(value);
 	};
 
 	return (
@@ -44,6 +60,7 @@ const ManageStudents = () => {
 						value={selectedClass}
 						onChangeHandler={onChangeHandler}
 					/>
+					<Students students={classStudents} />
 				</div>
 			) : (
 				<Spinner />
