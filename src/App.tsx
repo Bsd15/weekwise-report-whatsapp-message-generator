96946587cc26b3a5d3d6244a085261ff187f814e
bsd <datta.bathina15@gmail.com>
2020-07-10 15:49:07 +0530

    Changed div to secion tags Added section to contain NavLinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { NavLink, Route, Switch } from "react-router-dom";
-import ManageStudents from "./containers/ManageStudents/ManageStudents";
-import "./App.css";
+import React from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
+import ManageStudents from './containers/ManageStudents/ManageStudents';
+import './App.css';
 
 function App() {
 	return (
-		<div className="container">
-			<NavLink to="/manageStudents">Manage Students</NavLink>
+		<section className="container">
+			<section id="nav-links" className="bg-blue-100 w-full sm:w-1/3 flex justify-around px-3 py-3 sm:mx-auto">
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/manageStudents">Manage Students</NavLink>
+			</section>
 			<Switch>
 				<Route path="/manageStudents">
 					<ManageStudents />
 				</Route>
 			</Switch>
-		</div>
+		</section>
 	);
 }
 
