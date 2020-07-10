094790a87b3d0a89df6e501a219e973a38e1e2bb
bsd <datta.bathina15@gmail.com>
2020-07-10 22:34:14 +0530

    Added withAlert hoc

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import ManageStudents from './containers/ManageStudents/ManageStudents';
 import './App.css';
+import withAlert from './hoc/withAlert/withAlert';
 
 function App() {
 	return (
@@ -32,9 +33,29 @@ function App() {
 						<ManageStudents />
 					</Route>
 				</Switch>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem,
+					accusamus unde. Doloremque quia odio beatae iusto quidem sunt ab rerum
+					facere dicta, aliquid nulla natus eius optio quam praesentium.
+					Reprehenderit! Odio voluptatum facere non doloribus deleniti id
+					adipisci maiores sint assumenda mollitia, dolorem soluta architecto
+					nostrum harum consequuntur cupiditate, dolor veniam! Nemo odio quos
+					perspiciatis incidunt dolor recusandae laudantium deserunt? Totam
+					voluptates dolorem alias, illo commodi adipisci magni atque
+					repudiandae, minima minus aperiam! Qui explicabo tempore asperiores
+					doloribus, quo, in possimus, fugiat fugit aperiam ullam quisquam.
+					Neque, iusto dolorum? Inventore! Voluptas soluta, vero consectetur
+					maxime sint eligendi tenetur molestias saepe, sunt distinctio impedit,
+					corporis laudantium cumque ducimus quo reprehenderit doloribus ad
+					tempore dolore assumenda esse. Eaque nobis excepturi laudantium autem!
+					Ad quidem iste doloribus odio inventore ex odit commodi ea id iure.
+					Neque harum commodi eligendi repellendus velit! Facilis eius
+					necessitatibus commodi maiores voluptas sapiente amet repudiandae
+					laborum sit beatae!
+				</p>
 			</section>
 		</section>
 	);
 }
 
-export default App;
+export default withAlert(App);
