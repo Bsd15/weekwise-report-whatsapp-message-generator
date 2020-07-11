0efa2121ef6c58cd2a425822a8e8be094d4ea3e0
bsd <datta.bathina15@gmail.com>
2020-07-11 14:46:05 +0530

    Sent showAlert() to WrappedComponent as prop

--- src/hoc/withAlert/withAlert.tsx
+++ src/hoc/withAlert/withAlert.tsx
@@ -36,7 +36,7 @@ const withAlert = (WrappedComponent: React.ComponentType<any>) => {
 
 		return (
 			<>
-				<WrappedComponent {...props} />
+				<WrappedComponent {...props} showAlert={showAlert} />
 				<article
 					className={`fixed bottom-0 left-0 right-0 mx-auto container w-full lg:w-1/2 p-2 bg-white shadow-2xl border-t-8 ${alertBoxBorderClass} ${classes.alertBox} ${alertBoxShowClass}`}
 				>
