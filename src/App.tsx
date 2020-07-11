6ba4c8d468cd15f8ed6d38233385a7dac968116f
bsd <datta.bathina15@gmail.com>
2020-07-11 14:49:59 +0530

    Removed lorem ipsum from  #content

--- src/App.tsx
+++ src/App.tsx
@@ -33,26 +33,6 @@ function App() {
 						<ManageStudents />
 					</Route>
 				</Switch>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem,
-					accusamus unde. Doloremque quia odio beatae iusto quidem sunt ab rerum
-					facere dicta, aliquid nulla natus eius optio quam praesentium.
-					Reprehenderit! Odio voluptatum facere non doloribus deleniti id
-					adipisci maiores sint assumenda mollitia, dolorem soluta architecto
-					nostrum harum consequuntur cupiditate, dolor veniam! Nemo odio quos
-					perspiciatis incidunt dolor recusandae laudantium deserunt? Totam
-					voluptates dolorem alias, illo commodi adipisci magni atque
-					repudiandae, minima minus aperiam! Qui explicabo tempore asperiores
-					doloribus, quo, in possimus, fugiat fugit aperiam ullam quisquam.
-					Neque, iusto dolorum? Inventore! Voluptas soluta, vero consectetur
-					maxime sint eligendi tenetur molestias saepe, sunt distinctio impedit,
-					corporis laudantium cumque ducimus quo reprehenderit doloribus ad
-					tempore dolore assumenda esse. Eaque nobis excepturi laudantium autem!
-					Ad quidem iste doloribus odio inventore ex odit commodi ea id iure.
-					Neque harum commodi eligendi repellendus velit! Facilis eius
-					necessitatibus commodi maiores voluptas sapiente amet repudiandae
-					laborum sit beatae!
-				</p>
 			</section>
 		</section>
 	);
